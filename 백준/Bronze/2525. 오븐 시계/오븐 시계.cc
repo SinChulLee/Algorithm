8061eda394ed0d87152eed50a5aedfd3f097168a
이신철 <145883892+SinChulLee@users.noreply.github.com>
2024-05-10 14:24:21 +0900

    [Bronze III] Title: 오븐 시계, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int A, B, C;
+    cin >> A >> B >> C;
+
+    B += C;
+
+    if (B >= 60)
+    {
+        A += B / 60;
+        B = B % 60;
+        if (A >= 24)
+        {
+            A -= 24;
+        }
+    }
+
+    cout << A << " " << B;
+}
