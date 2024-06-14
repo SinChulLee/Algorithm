70410ec1c2b34a7d599e267134d74ed1292a5779
이신철 <145883892+SinChulLee@users.noreply.github.com>
2024-06-14 18:48:28 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, sum = 0, cnt = 0;
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        cnt++;
+    }
+    cout << cnt << endl
+         << 1;
+}
