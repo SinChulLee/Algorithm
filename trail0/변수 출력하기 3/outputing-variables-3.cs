40bcd836fdd526dcd17a3da82b54312d21f85e3a
이신철 <145883892+SinChulLee@users.noreply.github.com>
2026-08-23 13:18:17 +0900

    [Passed] 변수 출력하기 3 (75ms, 15MB)

---
+++ trail0/변수 출력하기 3/outputing-variables-3.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        int a = 1;
+        int b = 2;
+        string c = "C";
+
+        Console.WriteLine($"{a}->{b}->{c}");
+    }
+}
