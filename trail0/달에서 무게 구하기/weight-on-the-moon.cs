472699813823c452dea52e9a1a852682a4bb478b
이신철 <145883892+SinChulLee@users.noreply.github.com>
2026-08-23 13:24:26 +0900

    [Passed] 달에서 무게 구하기 (89ms, 14MB)

---
+++ trail0/달에서 무게 구하기/weight-on-the-moon.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        int c_weight = 13;
+        double m = 0.165;
+
+        Console.WriteLine($"{c_weight} * {m:F6} = {c_weight*m:F6}");
+    }
+}
