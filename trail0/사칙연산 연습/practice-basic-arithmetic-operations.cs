bebc520dd283725996d3ed42679838af01bad63f
이신철 <145883892+SinChulLee@users.noreply.github.com>
2026-08-23 13:13:15 +0900

    [Passed] 사칙연산 연습 (55ms, 5MB)

---
+++ trail0/사칙연산 연습/practice-basic-arithmetic-operations.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        int a = 5;
+        int b = 30;
+        int c = a + b;
+
+        Console.WriteLine(c/8);
+        Console.WriteLine(c%8);
+    }
+}
