058b5bfcaa8f2290a1be19ab36d4162583931aa7
이신철 <145883892+SinChulLee@users.noreply.github.com>
2026-08-23 13:30:30 +0900

    [Passed] 입력받아 계산 (64ms, 5MB)

---
+++ trail0/입력받아 계산/input-calculate.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        int a = int.Parse(Console.ReadLine());
+        Console.WriteLine(a+2);
+    }
+}
