8f7ece0a4dbe541e70e101c143a8d74e6f11abdd
이신철 <145883892+SinChulLee@users.noreply.github.com>
2026-08-23 13:46:43 +0900

    [Passed] 세 실수의 반올림 (107ms, 6MB)

---
+++ trail0/세 실수의 반올림/rounding-of-three-actual-numbers.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        float a = float.Parse(Console.ReadLine());
+        float b = float.Parse(Console.ReadLine());
+        float c = float.Parse(Console.ReadLine());
+
+        Console.WriteLine($"{a:F3}");
+        Console.WriteLine($"{b:F3}");
+        Console.WriteLine($"{c:F3}");
+    }
+}
