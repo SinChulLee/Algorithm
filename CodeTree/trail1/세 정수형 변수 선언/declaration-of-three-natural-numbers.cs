eef6ce60f15257328a08cda5d65d931204d0a6dc
이신철 <145883892+SinChulLee@users.noreply.github.com>
2026-08-24 21:34:56 +0900

    [Passed] 세 정수형 변수 선언 (57ms, 5MB)

---
+++ CodeTree/trail1/세 정수형 변수 선언/declaration-of-three-natural-numbers.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        int a = 7;
+        int b = 23;
+        int c = 30;
+
+        Console.Write($"{a} + {b} = {c}");
+    }
+}
