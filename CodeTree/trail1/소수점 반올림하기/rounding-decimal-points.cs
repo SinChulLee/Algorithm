73f08f28020e505ee327300e0dead7aa2ede4a26
이신철 <145883892+SinChulLee@users.noreply.github.com>
2026-08-24 21:52:04 +0900

    [Passed] 소수점 반올림하기 (77ms, 9MB)

---
+++ CodeTree/trail1/소수점 반올림하기/rounding-decimal-points.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        double a = 25.352;
+
+        Console.Write($"{a:F1}");
+    }
+}
