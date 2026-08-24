79e86aff57a69a6925cdd0ae39e0984dd66bc161
이신철 <145883892+SinChulLee@users.noreply.github.com>
2026-08-24 22:01:44 +0900

    [Passed] 길이 단위 변환하기 (80ms, 9MB)

---
+++ CodeTree/trail1/길이 단위 변환하기/change-length-unit.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        double ft = 30.48;
+        int mi = 160934;
+
+        Console.WriteLine($"9.2ft = {9.2*ft:F1}cm");
+        Console.WriteLine($"1.3mi = {1.3*mi:F1}cm");
+    }
+}
