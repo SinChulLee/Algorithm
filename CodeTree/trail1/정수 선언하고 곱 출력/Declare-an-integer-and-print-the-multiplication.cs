0908d0b12044faaa296e26a2cd39a77ccc1ba8d1
이신철 <145883892+SinChulLee@users.noreply.github.com>
2026-08-24 21:31:35 +0900

    [Passed] 정수 선언하고 곱 출력 (55ms, 5MB)

---
+++ CodeTree/trail1/정수 선언하고 곱 출력/Declare-an-integer-and-print-the-multiplication.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        int a = 26;
+        int b = 5;
+        Console.WriteLine($"{a} * {b} = {a*b}");
+    }
+}
