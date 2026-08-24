b8905a2646c95f5f18346e9e41e4fa5b0dc34cc7
이신철 <145883892+SinChulLee@users.noreply.github.com>
2026-08-24 21:08:11 +0900

    [Passed] 따옴표 출력 (67ms, 20MB)

---
+++ CodeTree/trail1/따옴표 출력/print-quote.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        Console.WriteLine("He says \"It's a really simple sentence\".");
+    }
+}
