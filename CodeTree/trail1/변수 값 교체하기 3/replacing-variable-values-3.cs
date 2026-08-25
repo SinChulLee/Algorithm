d680d2a6cbfafe3f3b1e8ccb36a5f6304d022feb
이신철 <145883892+SinChulLee@users.noreply.github.com>
2026-08-25 21:34:59 +0900

    [Passed] 변수 값 교체하기 3 (82ms, 20MB)

---
+++ CodeTree/trail1/변수 값 교체하기 3/replacing-variable-values-3.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        int a = 3;
+        int b = 5;
+        int c = a;
+        a = b; b = c;
+        Console.WriteLine(a);
+        Console.WriteLine(b);
+    }
+}
