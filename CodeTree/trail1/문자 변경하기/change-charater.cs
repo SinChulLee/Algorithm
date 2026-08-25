daeffa3c035fdadf12ec9be6efbde95a06aaf5aa
이신철 <145883892+SinChulLee@users.noreply.github.com>
2026-08-25 21:30:08 +0900

    [Passed] 문자 변경하기 (51ms, 4MB)

---
+++ CodeTree/trail1/문자 변경하기/change-charater.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        Char a = 'C';
+        a = 'T';
+        Console.Write(a);
+    }
+}
