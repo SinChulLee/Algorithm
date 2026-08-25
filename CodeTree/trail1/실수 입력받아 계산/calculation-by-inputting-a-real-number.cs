5fc6dcbc759944c61793d7be98299fbe835c1d09
이신철 <145883892+SinChulLee@users.noreply.github.com>
2026-08-25 22:00:44 +0900

    [Passed] 실수 입력받아 계산 (89ms, 5MB)

---
+++ CodeTree/trail1/실수 입력받아 계산/calculation-by-inputting-a-real-number.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        double a = double.Parse(Console.ReadLine());
+        double b = double.Parse(Console.ReadLine());
+        Console.Write($"{a+b:F2}");
+    }
+}
