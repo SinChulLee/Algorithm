b8093b892af1309264cb4baeea393a06b619ccbf
이신철 <145883892+SinChulLee@users.noreply.github.com>
2026-08-25 21:45:22 +0900

    [Passed] 점수 입출력 (64ms, 5MB)

---
+++ CodeTree/trail1/점수 입출력/enter-int-and-print-score.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        int N = int.Parse(Console.ReadLine());
+        Console.Write($"Your score is {N} point.");
+    }
+}
