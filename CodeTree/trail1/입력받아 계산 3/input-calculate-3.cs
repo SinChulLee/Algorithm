c833164b392d61f25bfee5053dce537041aaab2c
이신철 <145883892+SinChulLee@users.noreply.github.com>
2026-08-25 21:58:33 +0900

    [Passed] 입력받아 계산 3 (94ms, 9MB)

---
+++ CodeTree/trail1/입력받아 계산 3/input-calculate-3.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+        Console.Write(a*b);
+    }
+}
