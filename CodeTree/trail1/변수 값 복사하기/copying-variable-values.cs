e93e0a8e244142659d3d68f86c795bd87ab5ba93
이신철 <145883892+SinChulLee@users.noreply.github.com>
2026-08-25 21:40:34 +0900

    [Passed] 변수 값 복사하기 (55ms, 4MB)

---
+++ CodeTree/trail1/변수 값 복사하기/copying-variable-values.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        int a = 1;
+        int b = 2;
+        int c = 3;
+
+        a = c; b = c;
+        Console.WriteLine($"{a} {b} {c}");
+    }
+}
