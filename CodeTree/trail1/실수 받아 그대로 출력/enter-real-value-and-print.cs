b88893bbc789c15eec2f4bffe10969d8e57b0077
이신철 <145883892+SinChulLee@users.noreply.github.com>
2026-08-25 21:49:46 +0900

    [Passed] 실수 받아 그대로 출력 (86ms, 5MB)

---
+++ CodeTree/trail1/실수 받아 그대로 출력/enter-real-value-and-print.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        double N = double.Parse(Console.ReadLine());
+        Console.Write($"{N:F2}");
+    }
+}
