a2410fa0d00c2d436e2c4f45f1f70f185c515679
이신철 <145883892+SinChulLee@users.noreply.github.com>
2026-08-25 21:33:42 +0900

    [Passed] 정수 복사 (83ms, 22MB)

---
+++ CodeTree/trail1/정수 복사/copy-integer.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        int a = 3;
+        int b = 4;
+        b = a;
+
+        Console.WriteLine($"{a} {b}");
+        Console.WriteLine(a*b);
+    }
+}
