fc0ed3186efa9b9ef48cd694e4b05ea4d5821d7f
이신철 <145883892+SinChulLee@users.noreply.github.com>
2026-08-25 21:28:04 +0900

    [Passed] 두 실수의 곱 (92ms, 14MB)

---
+++ CodeTree/trail1/두 실수의 곱/the-product-of-two-real-numbers.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        double a = 5.26;
+        double b = 8.27;
+
+        Console.Write($"{a*b:F3}");
+    }
+}
