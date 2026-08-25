26461d7e7a5d84f91bb2fc243f2be2607f8b9a77
이신철 <145883892+SinChulLee@users.noreply.github.com>
2026-08-25 21:57:05 +0900

    [Passed] 입력받은 값과 합 출력 (66ms, 5MB)

---
+++ CodeTree/trail1/입력받은 값과 합 출력/output-of-value-and-sum.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        string[] input = Console.ReadLine().Split(' ');
+        int A = int.Parse(input[0]);
+        int B = int.Parse(input[1]);
+        Console.Write($"{A} {B} {A+B}");
+    }
+}
