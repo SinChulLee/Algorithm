c6ae51944f9c3b48d4604e7c3f44ce5b46fd9225
이신철 <145883892+SinChulLee@users.noreply.github.com>
2026-08-26 22:03:39 +0900

    [Passed] 큰 수에서 빼기 (64ms, 5MB)

---
+++ CodeTree/trail1/큰 수에서 빼기/subtract-from-large-number.cs
@@ -0,0 +1,20 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        string[] input = Console.ReadLine().Split(' ');
+        int A = int.Parse(input[0]);
+        int B = int.Parse(input[1]);
+        if (A > B)
+        {
+            Console.Write(A-B);
+        }
+        else
+        {
+            Console.Write(B-A);
+        }
+    }
+}
