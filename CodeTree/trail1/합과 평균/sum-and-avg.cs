4cf22ff2fbb8358d5f8ce1d16fbbe8999176dac1
이신철 <145883892+SinChulLee@users.noreply.github.com>
2026-08-26 21:53:06 +0900

    [Passed] 합과 평균 (84ms, 5MB)

---
+++ CodeTree/trail1/합과 평균/sum-and-avg.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        string[] input = Console.ReadLine().Split(' ');
+        int A = int.Parse(input[0]);
+        int B = int.Parse(input[1]);
+        Console.Write($"{A+B} {((double)(A+B)/2):F1}");
+    }
+}
