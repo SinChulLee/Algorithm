070cf0c5c8113768d89cc694593baced8a205d72
이신철 <145883892+SinChulLee@users.noreply.github.com>
2026-08-26 21:20:49 +0900

    [Passed] 문자열 순서 바꾸기 (61ms, 5MB)

---
+++ CodeTree/trail1/문자열 순서 바꾸기/change-order-of-strings.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        string S = Console.ReadLine();
+        string T = Console.ReadLine();
+        Console.WriteLine(T);
+        Console.WriteLine(S);
+    }
+}
