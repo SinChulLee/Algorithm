63e977d711686919c4f7f33af4bf4a06e0cb58c0
이신철 <145883892+SinChulLee@users.noreply.github.com>
2026-08-26 21:16:34 +0900

    [Passed] 문자 받아 출력 (56ms, 5MB)

---
+++ CodeTree/trail1/문자 받아 출력/enter-char-and-print.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        string input = (Console.ReadLine());
+        char c = input[0];
+        Console.Write(c);
+    }
+}
