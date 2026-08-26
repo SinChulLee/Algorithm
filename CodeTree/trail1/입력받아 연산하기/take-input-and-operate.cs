0e0736b664bbe214a478b2c49e88be235a3159fe
이신철 <145883892+SinChulLee@users.noreply.github.com>
2026-08-26 21:40:32 +0900

    [Passed] 입력받아 연산하기 (67ms, 6MB)

---
+++ CodeTree/trail1/입력받아 연산하기/take-input-and-operate.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+        Console.WriteLine(a+87);
+        Console.WriteLine(b%10);
+    }
+}
