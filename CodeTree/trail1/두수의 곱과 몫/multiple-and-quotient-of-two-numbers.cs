7bacced87c5391646a7e12cd67bd6ddb91ff0077
이신철 <145883892+SinChulLee@users.noreply.github.com>
2026-08-26 21:36:35 +0900

    [Passed] 두수의 곱과 몫 (67ms, 5MB)

---
+++ CodeTree/trail1/두수의 곱과 몫/multiple-and-quotient-of-two-numbers.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        string[] input = Console.ReadLine().Split(' ');
+        int a = int.Parse(input[0]);
+        int b = int.Parse(input[1]);
+        Console.WriteLine($"{a} * {b} = {a*b}");
+        Console.WriteLine($"{a} / {b} = {a/b}");
+    }
+}
