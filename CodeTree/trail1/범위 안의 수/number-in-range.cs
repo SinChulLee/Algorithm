2c70d258158040790e56c199260cbe45d44ec3c6
이신철 <145883892+SinChulLee@users.noreply.github.com>
2026-08-27 22:03:28 +0900

    [Passed] 범위 안의 수 (64ms, 5MB)

---
+++ CodeTree/trail1/범위 안의 수/number-in-range.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        int a = int.Parse(Console.ReadLine());
+
+        if (a >= 10 && a <= 20)
+            Console.WriteLine("yes");
+        else
+            Console.WriteLine("no");
+    }
+}
