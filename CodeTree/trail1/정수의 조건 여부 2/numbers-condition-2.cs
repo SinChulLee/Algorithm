23fe4c9f60672292d6460f278c81767f21d3db97
이신철 <145883892+SinChulLee@users.noreply.github.com>
2026-08-27 21:38:12 +0900

    [Passed] 정수의 조건 여부 2 (65ms, 5MB)

---
+++ CodeTree/trail1/정수의 조건 여부 2/numbers-condition-2.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        int a = int.Parse(Console.ReadLine());
+
+        if (a == 5)
+            Console.WriteLine("A");
+
+        if (a % 2 == 0)
+            Console.WriteLine("B");
+    }
+}
