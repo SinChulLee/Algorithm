126cb7018ee33827ed03497e10f2f42bb14a75fe
이신철 <145883892+SinChulLee@users.noreply.github.com>
2026-08-27 21:52:53 +0900

    [Passed] 비교 연산 (65ms, 5MB)

---
+++ CodeTree/trail1/비교 연산/comparison-operator.cs
@@ -0,0 +1,42 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        string[] input = Console.ReadLine().Split(' ');
+        int A = int.Parse(input[0]);
+        int B = int.Parse(input[1]);
+
+        if (A >= B)
+            Console.WriteLine(1);
+        else
+            Console.WriteLine(0);
+
+        if (A > B)
+            Console.WriteLine(1);
+        else
+            Console.WriteLine(0);
+
+        if (B >= A)
+            Console.WriteLine(1);
+        else
+            Console.WriteLine(0);
+
+        if (B > A)
+            Console.WriteLine(1);
+        else
+            Console.WriteLine(0);
+
+        if (A == B)
+            Console.WriteLine(1);
+        else
+            Console.WriteLine(0);
+
+        if (A != B)
+            Console.WriteLine(1);
+        else
+            Console.WriteLine(0);
+    }
+}
