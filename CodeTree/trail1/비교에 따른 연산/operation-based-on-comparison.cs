5a3ff8bd025e7cd3b3a3f1d520d99236da152a96
이신철 <145883892+SinChulLee@users.noreply.github.com>
2026-08-27 21:17:38 +0900

    [Passed] 비교에 따른 연산 (81ms, 27MB)

---
+++ CodeTree/trail1/비교에 따른 연산/operation-based-on-comparison.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        string[] input = Console.ReadLine().Split(' ');
+        int a = int.Parse(input[0]);
+        int b = int.Parse(input[1]);
+        if (a > b)
+            Console.Write(a*b);
+        else
+            Console.Write(b/a);
+    }
+}
