72d192c6d562142947996e9663bc5635212c8b29
이신철 <145883892+SinChulLee@users.noreply.github.com>
2026-08-27 21:23:14 +0900

    [Passed] 삼항연산자 2 (62ms, 5MB)

---
+++ CodeTree/trail1/삼항연산자 2/ternary-operator-2.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        int a = int.Parse(Console.ReadLine());
+        Console.Write(a == 1 ? "t" : "f");
+    }
+}
