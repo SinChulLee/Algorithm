c0c6da644f1c84dcd9c13f810f9ad72916b3046f
이신철 <145883892+SinChulLee@users.noreply.github.com>
2026-08-28 16:29:43 +0900

    [Passed] 짝수만 출력 (64ms, 5MB)

---
+++ CodeTree/trail1/짝수만 출력/output-only-even.cs
@@ -0,0 +1,19 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        string[] input = Console.ReadLine().Split(' ');
+        int A = int.Parse(input[0]);
+        int B = int.Parse(input[1]);
+
+        while(A <= B)
+        {
+            if (A % 2 == 0)
+                Console.Write($"{A} ");
+            A++;
+        }
+    }
+}
