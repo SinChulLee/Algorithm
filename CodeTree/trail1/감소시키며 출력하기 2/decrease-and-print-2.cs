ebca3d2910ee8047770deaa3f9f36b35bee01190
이신철 <145883892+SinChulLee@users.noreply.github.com>
2026-08-28 16:31:38 +0900

    [Passed] 감소시키며 출력하기 2 (60ms, 4MB)

---
+++ CodeTree/trail1/감소시키며 출력하기 2/decrease-and-print-2.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        int Count = 26;
+        while (Count >= 10)
+        {
+            Console.Write($"{Count} "); 
+            Count -= 1;
+        }
+    }
+}
