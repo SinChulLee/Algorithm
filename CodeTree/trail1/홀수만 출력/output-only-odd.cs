0404bb49018c8c53165aff07b4dab31c20429564
이신철 <145883892+SinChulLee@users.noreply.github.com>
2026-08-28 16:12:41 +0900

    [Passed] 홀수만 출력 (75ms, 5MB)

---
+++ CodeTree/trail1/홀수만 출력/output-only-odd.cs
@@ -0,0 +1,19 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        string[] input = Console.ReadLine().Split(' ');
+
+        int A = int.Parse(input[0]);
+        int B = int.Parse(input[1]);
+
+        for (int i = A; i <= B; i++)
+        {
+            if (i % 2 != 0)
+                Console.Write($"{i} ");
+        }
+    }
+}
