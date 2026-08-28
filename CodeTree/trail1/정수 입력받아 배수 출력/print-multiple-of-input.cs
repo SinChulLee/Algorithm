5ec1f6cdd7e252adbeeda48d444cccbb4e435d29
이신철 <145883892+SinChulLee@users.noreply.github.com>
2026-08-28 16:10:27 +0900

    [Passed] 정수 입력받아 배수 출력 (70ms, 5MB)

---
+++ CodeTree/trail1/정수 입력받아 배수 출력/print-multiple-of-input.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        int N = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= 5; i++)
+        {
+            Console.Write($"{N*i} ");
+        }
+    }
+}
