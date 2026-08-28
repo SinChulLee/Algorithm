e82805b870ac876081e69249bcd21a310d03add3
이신철 <145883892+SinChulLee@users.noreply.github.com>
2026-08-28 17:46:58 +0900

    [Passed] 문자 출력하기 (59ms, 5MB)

---
+++ CodeTree/trail1/문자 출력하기/print-text.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        string A = Console.ReadLine();
+
+        for (int i = 1; i <= 8; i++)
+        {
+            Console.Write(A);
+        }
+    }
+}
