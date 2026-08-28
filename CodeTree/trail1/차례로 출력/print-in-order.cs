63ed51f21a3c59c8284505ec2293ebc82a6bd2d3
이신철 <145883892+SinChulLee@users.noreply.github.com>
2026-08-28 15:49:57 +0900

    [Passed] 차례로 출력 (58ms, 5MB)

---
+++ CodeTree/trail1/차례로 출력/print-in-order.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        Console.Write($"5");
+        for (int i = 6; i < 18; i++)
+        {
+
+            Console.Write($" {i}");
+        }
+    }
+}
