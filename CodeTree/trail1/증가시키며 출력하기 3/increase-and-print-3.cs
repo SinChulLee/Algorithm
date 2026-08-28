af09ac4af22b87891ea0e6cce6355550c82ae44e
이신철 <145883892+SinChulLee@users.noreply.github.com>
2026-08-28 16:25:44 +0900

    [Passed] 증가시키며 출력하기 3 (60ms, 4MB)

---
+++ CodeTree/trail1/증가시키며 출력하기 3/increase-and-print-3.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        int Count = 10;
+        while (Count <= 26)
+        {
+            Console.Write($"{Count} "); 
+            Count += 2;
+        }
+    }
+}
