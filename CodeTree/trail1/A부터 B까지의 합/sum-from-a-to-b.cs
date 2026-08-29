8f2fb671f28508eb7580fc05e6e37ef067dd3ae0
이신철 <145883892+SinChulLee@users.noreply.github.com>
2026-08-29 22:50:38 +0900

    [Passed] A부터 B까지의 합 (65ms, 5MB)

---
+++ CodeTree/trail1/A부터 B까지의 합/sum-from-a-to-b.cs
@@ -0,0 +1,21 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        string[] input = Console.ReadLine().Split(' ');
+
+        int A = int.Parse(input[0]);
+        int B = int.Parse(input[1]);
+
+        int ans = 0;
+        for (int i = A; i <= B; i++)
+        {
+            ans += i;
+        }
+
+        Console.Write(ans);
+    }
+}
