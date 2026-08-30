c237715c5d5bf22b9f39fa6ab44545b74403879f
이신철 <145883892+SinChulLee@users.noreply.github.com>
2026-08-30 12:51:58 +0900

    [Passed] A부터 B까지 짝수의 합 (64ms, 5MB)

---
+++ CodeTree/trail1/A부터 B까지 짝수의 합/sum-of-even-nums-from-a-to-b.cs
@@ -0,0 +1,22 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        string[] input = Console.ReadLine().Split(' ');
+        int A = int.Parse(input[0]);
+        int B = int.Parse(input[1]);
+
+        int sum = 0;
+        for (int i = A; i <= B; i++)
+        {
+            if (i % 2 == 0)
+            {
+                sum += i;
+            }
+        }
+        Console.Write(sum);
+    }
+}
