c61ed844dbe695339d39a6a0a4b94baa27087c70
이신철 <145883892+SinChulLee@users.noreply.github.com>
2026-08-30 16:27:30 +0900

    [Passed] 0 나올때까지 출력 (62ms, 5MB)

---
+++ CodeTree/trail1/0 나올때까지 출력/print-until-zero-is-given.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        while(true)
+        {
+            int N = int.Parse(Console.ReadLine());
+            if (N == 0)
+                break;
+            Console.WriteLine(N);
+        }
+    }
+}
