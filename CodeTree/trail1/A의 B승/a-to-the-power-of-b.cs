ef14c6e1933304f82991c58219eec24f416c0f3c
이신철 <145883892+SinChulLee@users.noreply.github.com>
2026-08-30 13:19:04 +0900

    [Passed] A의 B승 (64ms, 5MB)

---
+++ CodeTree/trail1/A의 B승/a-to-the-power-of-b.cs
@@ -0,0 +1,20 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        string[] input = Console.ReadLine().Split(' ');
+        int A = int.Parse(input[0]);
+        int B = int.Parse(input[1]);
+        int ans = 1;
+
+        for (int i = 1; i <= B; i++)
+        {
+            ans *= A;
+        }
+
+        Console.Write(ans);
+    }
+}
