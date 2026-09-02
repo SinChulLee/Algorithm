d664e7208fe2bb1954d1afcb6c193a1b28e93e54
이신철 <145883892+SinChulLee@users.noreply.github.com>
2026-09-02 14:01:49 +0900

    [Passed] 별표 출력하기 (64ms, 5MB)

---
+++ CodeTree/trail1/별표 출력하기/print-star.cs
@@ -0,0 +1,24 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        int N = int.Parse(Console.ReadLine());
+
+        for(int i = 1; i <= N; i++)
+        {
+            for(int j = 1; j <= i; j++)
+                Console.Write("* ");
+            Console.WriteLine();
+        }
+
+        for(int i = N-1; i > 0; i--)
+        {
+            for(int j = 1; j <= i; j++)
+                Console.Write("* ");
+            Console.WriteLine();
+        }
+    }
+}
