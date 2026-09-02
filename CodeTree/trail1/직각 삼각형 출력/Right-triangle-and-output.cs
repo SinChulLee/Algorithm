c3d2a19e9068e1ea09ff06b7b33d8f7cf6f07743
이신철 <145883892+SinChulLee@users.noreply.github.com>
2026-09-02 13:47:03 +0900

    [Passed] 직각 삼각형 출력 (65ms, 5MB)

---
+++ CodeTree/trail1/직각 삼각형 출력/Right-triangle-and-output.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        int N = int.Parse(Console.ReadLine());
+        for(int i = 1; i <= N; i++)
+        {
+            for(int j = 0; j < (2*i)-1; j++)
+                Console.Write('*');
+            Console.WriteLine();
+        }
+    }
+}
