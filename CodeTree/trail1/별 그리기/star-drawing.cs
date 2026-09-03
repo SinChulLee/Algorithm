1219a34b3b95e69d9f529b6cb84d466a138026ff
이신철 <145883892+SinChulLee@users.noreply.github.com>
2026-09-03 19:56:56 +0900

    [Passed] 별 그리기 (64ms, 5MB)

---
+++ CodeTree/trail1/별 그리기/star-drawing.cs
@@ -0,0 +1,36 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        int N = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j < N - 1 - i; j++)
+            {
+                Console.Write(" ");
+            }
+            for (int k = 0; k < 2 * i + 1; k++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+
+        for (int i = 1; i < N; i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                Console.Write(" ");
+            }
+            for (int k = 0; k < 2 * N - 2 * i - 1; k++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+    }
+}
