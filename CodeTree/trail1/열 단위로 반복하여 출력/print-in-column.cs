06766864c24fbf9357fd1dab8b81fe67c93f438b
이신철 <145883892+SinChulLee@users.noreply.github.com>
2026-09-06 17:43:47 +0900

    [Passed] 열 단위로 반복하여 출력 (84ms, 10MB)

---
+++ CodeTree/trail1/열 단위로 반복하여 출력/print-in-column.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        int N = int.Parse(Console.ReadLine());
+
+        for(int i = 1; i <= N; i++)
+        {
+            for (int j = 0; j < N; j++)
+                Console.Write(i);    
+            Console.WriteLine();
+        }
+    }
+}
