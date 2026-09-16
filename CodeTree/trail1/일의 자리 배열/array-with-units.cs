c2dcbb7e6089e14656a4c41429c9ec8554acd959
이신철 <145883892+SinChulLee@users.noreply.github.com>
2026-09-16 17:12:34 +0900

    [Passed] 일의 자리 배열 (66ms, 5MB)

---
+++ CodeTree/trail1/일의 자리 배열/array-with-units.cs
@@ -0,0 +1,24 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        string[] input = Console.ReadLine().Split(' ');
+        int[] arr = new int[10];
+        arr[0] = int.Parse(input[0]);
+        arr[1] = int.Parse(input[1]);
+
+        for(int i = 2; i < 10; i++)
+        {
+            int sum = arr[i-2] + arr[i-1];
+            arr[i] = sum % 10;
+        }
+
+        for(int j = 0; j < 10; j++)
+        {
+            Console.Write($"{arr[j]} ");
+        }
+    }
+}
